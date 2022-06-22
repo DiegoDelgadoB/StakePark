8e47df8c698e7d04bc2b1d80a53d25fb253c6e86
Diego Delgado Barra <diego.delgadobarra@outlook.com>
2022-06-22 15:45:49 -0400

    ThirdCommit

--- consultas.js
+++ consultas.js
@@ -23,3 +23,69 @@ const insertarSkater = async (skater) => {
         return error;
     }
 };
+
+const consultarSkaters = async () => {
+    try {
+        const query = "SELECT * FROM skaters";
+        const resp = await pool.query(query);
+        return resp;
+    } catch (error) {
+        return error;
+    }
+};
+
+const consultarSkater = async (email, password) => {
+    try {
+        const query = `SELECT * FROM skaters WHERE email = "${email}" AND password = "${password}"`;
+        const resp = await pool.query(query);
+        return resp;
+    } catch (error) {
+        return error;
+    }
+};
+
+const actualizarSkater = async (skater) => {
+    const { nombre, password, anos_experiencia, especialidad } = skater;
+
+    const config = {
+        text: "UPDATE skaters SET nombre = $1, password = $2, anos_experiencia = $3, especialidad = $4 RETURNING *",
+        values: [nombre, password, anos_experiencia, especialidad]
+    }
+
+    try {
+        const resp = await pool.query(config);
+        return resp;
+    } catch (error) {
+        return error;
+    }
+};
+
+const actualizarEstadoSkater = async (id, estado) => {
+    const config = {
+        text: "UPDATE skaters SET estado = $1 WHERE id = $2 RETURNING *",
+        values: [estado, id]
+    }
+
+    try {
+        const resp = await pool.query(config);
+        return resp;
+    } catch (error) {
+        return error;
+    }
+};
+
+const eliminarSkater = async (id) => {
+    const config = {
+        text: "DELETE FROM skaters WHERE id = $1 RETURNING *",
+        values: [id]
+    }
+
+    try {
+        const resp = await pool.query(config);
+        return resp;
+    } catch (error) {
+        return error;
+    }
+};
+
+module.exports = { insertarSkater, consultarSkaters, consultarSkater, actualizarSkater, actualizarEstadoSkater, eliminarSkater }
